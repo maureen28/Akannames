786fc3faee2dcaa42ed490f03e306cde836d4a21
Maureen <murimimaureen69@gmail.com>
2019-10-25 15:37:01 +0300

    add bootstrap and css files

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@ var date=parseInt(prompt("Enter date of birth?"))
 var month=parseInt(prompt("Enter month of birth?"))
 var year=parseInt(prompt("Enter year of birth?"))
 var century=parseInt(prompt("Enter year of birth?"))
+var gender=prompt("Enter ")
 
 var date=document.getElementById('date').value;
 var month=document.getElementById('month').value;
 var year=document.getElementById('year').value;
+var gender=document.getElementById('gender').value;
 
-var day=(century/4)-2*century-1)+((5*year/4)+((26*(month+1)/10))+date)%7
+var day=((century/4)-2*century-1)+((5*year/4)+((26*(month+1)/10))+date)%7
 var maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
 var femaleNames=['Akosua','Adwoa','Abenna','Akua','Yaa','Afua','Ama']
 
-    }
