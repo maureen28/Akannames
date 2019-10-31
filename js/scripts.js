85859f2e3a147177bac0a96b9bbd217ec5e41c92
Maureen <murimimaureen69@gmail.com>
2019-10-31 13:11:12 +0300

    update changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
-function xx() {
+function x() {
     var daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
     var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
     var femaleNames = ['Akosua', 'Adwoa', 'Abenna', 'Akua', 'Yaa', 'Afua', 'Ama']
 
-    var date = parseInt(document.getElementById('date').value);
-    var month = parseInt(document.getElementById('month').value);
-    var year = parseInt(document.getElementById('year').value);
-    var genderM = document.getElementById('rd1');
-    var genderF = document.getElementById('rd2');
+    var date = parseInt(document.getElementById("date").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var century = parseInt(document.getElementById("century").value);
+    document.getElementById('rd1');
+    document.getElementById('rd2');
 
-    alert("Please enter details")
-
-    if (year == "") {
+    // if (submit.clicked == true)
+    //     alert("Please enter details");
+    if (century == "") {
+        alert("Input the correct century");
+    } else if (year == "") {
         alert("Input the correct year");
     } else if (month >= 12) {
         alert("Input the correct month");
@@ -19,7 +22,7 @@ function xx() {
         alert("Input the correct date");
     }
     else {
-        var day = Math.trunc(parseInt(-(((year / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date)) % 7);
+        var day = Math.trunc(parseInt((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date)) % 7);
     }
 
     if (rd1.checked == true) {
@@ -31,8 +34,5 @@ function xx() {
 
     }
 }
-    
 
-
-}
 
